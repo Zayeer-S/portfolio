132e3db2c2e47a2b50d167c7a12c1872bbf8e436
A-abd <65906361+A-abd@users.noreply.github.com>
2025-09-23 16:11:39 +0100

    style: make desktop icons slightly smaller and more square like

--- src/components/desktop/DesktopIcon.tsx
+++ src/components/desktop/DesktopIcon.tsx
@@ -3,10 +3,10 @@ import { DesktopIconProps } from '@/types';
 export default function DesktopIcon({ icon, label, onClick }: DesktopIconProps) {
   return (
     <div
-    className="flex flex-col items-center p-2 hover:bg-gray-500/30 cursor-pointer rounded group"      
+    className="flex flex-col items-center px-1 py-1 hover:bg-gray-500/30 cursor-pointer rounded group"      
     onDoubleClick={onClick}
     >
-      <div className="w-12 h-12 mb-1 flex items-center justify-center text-5xl">
+      <div className="w-10 h-12 mb-1 flex items-center justify-center text-5xl">
         {icon}
       </div>
       <span className="text-white text-xs text-center group-hover px-1 rounded">
